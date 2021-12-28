0d1348ecbad69d703c41d564ceb2b949521376cb
Abhinav <abhinav.mishra@gmail.com>
2021-12-28 15:33:22 +0530

    files api added

--- models/directory.go
+++ models/directory.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"encoding/json"
+	"log"
+)
+
 type Directory struct {
 	ID    int    `json:"id"`
 	Name  string `json:"name"`
@@ -11,4 +16,22 @@ type File struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 	Path string `json:"path"`
+}
+
+type ThetvdbLogin struct {
+	Status string `json:"status"`
+	Data   data   `json:"data"`
+}
+
+type data struct {
+	Token string `json:"token"`
+}
+
+func (d *ThetvdbLogin) UnMarshal(data string) ThetvdbLogin {
+	res := ThetvdbLogin{}
+	err := json.Unmarshal([]byte(data), &res)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return res
 }
--- routes/file-rotes.go
+++ routes/file-rotes.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"gin1gorm/models"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"strings"
 	"sync"
 
 	"github.com/gin-gonic/gin"
@@ -14,7 +19,11 @@ var wg sync.WaitGroup
 func GetDir() func(c *gin.Context) {
 	return func(c *gin.Context) {
 		var directory []models.Directory
-
+		chanel := make(chan string)
+		wg.Add(1)
+		go tv_db_login(chanel)
+		bearerToken := "Bearer " + <-chanel
+		log.Println("login", bearerToken)
 		files, err := ioutil.ReadDir("/run/user/1000/gvfs/smb-share:server=synology1.local,share=abhinavssynologyvol1/TV Shows")
 		if err != nil {
 			c.JSON(400, gin.H{"error": err.Error()})
@@ -22,8 +31,11 @@ func GetDir() func(c *gin.Context) {
 			for i, file := range files {
 				log.Println(file.Name())
 				ch := make(chan []models.File)
+				ch1 := make(chan string)
 				wg.Add(1)
+				go getSeries(file.Name(), bearerToken, ch1)
 				go getFiles("/run/user/1000/gvfs/smb-share:server=synology1.local,share=abhinavssynologyvol1/TV Shows/"+file.Name(), ch)
+
 				d := models.Directory{ID: i, Name: file.Name(), Path: "/run/user/1000/gvfs/smb-share:server=synology1.local,share=abhinavssynologyvol1/TV Shows/" + file.Name(), Files: <-ch}
 				directory = append(directory, d)
 			}
@@ -34,6 +46,52 @@ func GetDir() func(c *gin.Context) {
 	}
 }
 
+func tv_db_login(channel chan<- string) {
+	values := map[string]string{"apikey": "9782b4fb-c262-4143-b983-acfc19c679e8",
+		"pin": "7HHCJSWV"}
+	json_data, err := json.Marshal(values)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := http.Post("http://api4.thetvdb.com/v4/login", "application/json", bytes.NewBuffer(json_data))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var res models.ThetvdbLogin
+	r := res.UnMarshal(string(body))
+	channel <- r.Data.Token
+	close(channel)
+}
+
+func getSeries(name string, bearerToken string, ch1 chan<- string) {
+	url := fmt.Sprintf("https://api4.thetvdb.com/v4/search?q=%s", strings.Replace(name, " ", "%20", -1))
+	log.Printf("url: %s", url)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	req.Header.Add("Authorization", bearerToken)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(string(body))
+	ch1 <- string(body)
+	close(ch1)
+
+}
 func getFiles(path string, ch chan<- []models.File) {
 	files, err := ioutil.ReadDir(path)
 	var Files []models.File
